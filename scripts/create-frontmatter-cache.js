0de802cca0eb72309861b9be6f194802beb837f8
Aleksander Tidemann <aleksandertid@gmail.com>
2023-05-27 20:23:22 +0200

    removed all related to projects

--- scripts/create-frontmatter-cache.js
+++ scripts/create-frontmatter-cache.js
@@ -34,10 +34,11 @@ function getAllCacheFr(postType = "posts") {
 
 // gather all frontmatter and make one big list
 const posts = getAllCacheFr("posts");
-const projects = getAllCacheFr("projects");
-const allFr = posts.concat(projects);
+// const projects = getAllCacheFr("projects");
+// const allFr = posts.concat(projects);
 
-const fileContents = `export const frontMatterCache = ${JSON.stringify(allFr)}`;
+// const fileContents = `export const frontMatterCache = ${JSON.stringify(allFr)}`;
+const fileContents = `export const frontMatterCache = ${JSON.stringify(posts)}`;
 
 // if the cache directory does not exist, create it
 try {
