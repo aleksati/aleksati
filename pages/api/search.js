3c0ddda6756316c50e3a63d9907f281a8fc530b0
Aleksander Tidemann <aleksandertid@gmail.com>
2023-08-14 11:15:35 +0200

    Update search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,6 +2,13 @@ import { initValidation, get, query } from "../../middleware/middlewareApi";
 import { frontMatterCache } from "../../cache/frontmatter";
 import nextConnect from "next-connect";
 
+// maybe send a "mounted" or "get-ready" request to initalize the API route.
+// I can send it when a users highlights the search. so on Focus.
+// but first, try this:
+// https://nextjs.org/docs/pages/building-your-application/routing/api-routes#edge-api-routes
+
+// handles my page search function.
+
 const searchValidation = initValidation([
   query("q").exists().withMessage("search request must include query (q)."),
 ]);
@@ -33,11 +40,11 @@ export default nextConnect()
       }
 
       // then summary
-      if (!results.length) {
-        results = frontMatterCache.filter((fr) =>
-          fr.summary.toLowerCase().includes(query.toLowerCase())
-        );
-      }
+      // if (!results.length) {
+      //   results = frontMatterCache.filter((fr) =>
+      //     fr.summary.toLowerCase().includes(query.toLowerCase())
+      //   );
+      // }
     }
 
     res.status(200).json({ results });
