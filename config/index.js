0c06ee9318a308536578f8d08c150f8759fc690c
Aleksander Tidemann <aleksandertid@gmail.com>
2023-12-11 18:31:33 +0100

    Post updates

--- config/index.js
+++ config/index.js
@@ -33,13 +33,13 @@ export const KEYWORDS_2_COLOR = {
   javascript: "#a8a29e", // stone-400
   // starting over!
   sonification: "#3f6212", // lime-800
-  data: "#1e40af", // blue-800
+  "data-science": "#1e40af", // blue-800
   telenor: "#991b1b", // red-800
   latency: "#6b21a8", //purple-800
   telematic: "#854d0e", //yellow-800
   jazz: "#155e75", //cyan-800
   p5js: "#115e59", // teal-800
-  lola: "#075985", //sky-800
+  // lola: "#075985", //sky-800
   osc: "#9a3412", //orange-800
   software: "#3730a3", //indigo-800
   realtime: "#5b21b6", //violet-800
@@ -48,7 +48,7 @@ export const KEYWORDS_2_COLOR = {
   jitter: "#9d174d", //pink-800
   band: "#86198f", //fuchsia-800
   sculpture: "#92400e", //amber-800
-  // vocals: "#292524", // stone-800
+  quality: "#292524", // stone-800
   // starting over !!
   pop: "#ecfccb", //lime-100
   drums: "#fee2e2", //red-100
@@ -57,18 +57,16 @@ export const KEYWORDS_2_COLOR = {
   kuramoto: "#cffafe", //cyan-100
   improvisation: "#ccfbf1", //teal-100
   installation: "#e0f2fe", //sky-100
-  spectral: "#ffedd5", //orange-100
+  //spectral: "#ffedd5", //orange-100
   visuals: "#e0e7ff", //indigo-100
   art: "#ede9fe", //violet-100
   rock: "#d1fae5", // emerald-100
   programming: "#ffe4e6", //rose-100
-  //bass: "#fce7f3", //pink-100
+  jacktrip: "#fce7f3", //pink-100
   "machine-learning": "#fae8ff", //fuchsia-100
   // tech: "#e879f9", // Fuschsia-400
   electronic: "#fef3c7", //amber-100 //
-  digital: "#dbeafe", //blue-100
+  // digital: "#dbeafe", //blue-100
   "sp-tools": "#115e59", //teal-800
   mapping: "#f5f5f4", // stone-100
 };
-
-// programming
