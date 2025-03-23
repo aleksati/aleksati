e9df64ca67ff1a3b4ac3889408ff2a2703c58ae0
Aleksander Tidemann <aleksandertid@gmail.com>
2025-03-23 21:08:34 +0100

    New Post!

--- src/templates/PostListSmall.tsx
+++ src/templates/PostListSmall.tsx
@@ -2,7 +2,7 @@ import PostListItemSmall from "../components/PostListItemSmall";
 
 const PostListSmall = ({ frontMatterList }: { frontMatterList: FrontMatterList }) => {
   return (
-    <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 gap-4">
       {frontMatterList.map((frontMatter) => (
         <PostListItemSmall key={frontMatter.slug} {...frontMatter} />
       ))}
