3a9c8b1b3ea9bdb1c4ca53c7d00a0da89492dc42
Aleksander Tidemann <aleksandertid@gmail.com>
2025-04-11 07:44:04 +0200

    Sun updates

--- src/pages/sun.tsx
+++ src/pages/sun.tsx
@@ -2,35 +2,41 @@ import { GetStaticProps } from "next";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
-// the-sun-rn
-// with spinner
+// thesunrightnow
 
 export default function handler() {
   const [currIdx, setCurrIdx] = useState<number>(0);
   const [imgFileNames, setImgFileNames] = useState<string[]>([""]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isReady, setIsReady] = useState<boolean>(false);
 
   const getImgFileNames = async () => {
     try {
-      // loading (showLoading) show loading
+      setIsLoading(true);
       const res = await fetch("./api/sun");
       const data = await res.json();
-      console.log(data)
+      console.log(data);
       setImgFileNames(data);
+      setIsLoading(false);
+      setIsReady(true)
     } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+      setIsReady(false);
       console.log(error);
     }
   };
 
   useEffect(() => {
     // on mount
-    getImgFileNames();
+    //get new filenames from API every 10 minutes
+    const interval = setInterval(() => {
+      getImgFileNames();
+    }, 600000);
 
-    // get new filenames from API every 10 minutes
-    // const interval = setInterval(() => {
-    //   getNewImgFileNames();
-    // }, 600000);
-
-    // return () => clearInterval(interval)
+    // cleanup on unmount
+    return () => clearInterval(interval)
   }, []);
 
   useEffect(() => {
@@ -45,21 +51,33 @@ export default function handler() {
 
   return (
     <div className="flex flex-col min-h-screen bg-black">
-      <div className="flex flex-grow items-center justify-center">
-        <Image
-          src={imgFileNames[currIdx]}
-          width="512"
-          height="512"
-          object-fit="cover"
-          alt="Sun Image"
-        />
+      <div>
+        <p className="text-xs text-secondary-dark p-2">
+          The sun right now. Images sourced from{" "}
+          <a className="text-blue-300" href="https://sdo.gsfc.nasa.gov/">
+            NASA SDO
+          </a>
+          . Updates every 5 minutes.
+        </p>
       </div>
-        <div>
-          <p className="text-xs text-center">
-            The sun right now. Images from <a className="text-blue-300" href="https://sdo.gsfc.nasa.gov/">NASA SDO</a>. Updates every 5
-            minutes.
-          </p>
-        </div>
+      {isLoading ? <p className=" px-2 text-xs text-secondary-dark">Loading new images...</p> : <></>}
+      <div className="flex flex-grow items-center justify-center">
+        {isError ? (
+          <p>Obs! Something went wrong</p>
+        ) : !isReady ? (
+          <></>
+        ) : (
+          <Image
+            src={imgFileNames[currIdx]}
+            width="512"
+            height="512"
+            object-fit="cover"
+            alt="Sun Image"
+            quality={100}
+            priority
+          />
+        )}
+      </div>
     </div>
   );
 }
