e653a1dfb2cdb540f58eba930a63ed8838f15cf2
Aleksander Tidemann <aleksandertid@gmail.com>
2025-08-27 10:44:31 +0200

    Few  more style updates

--- src/templates/About.tsx
+++ src/templates/About.tsx
@@ -4,18 +4,18 @@ import MySoMeBar from "../components/MySoMeBar";
 
 const About = () => (
   <div className="pb-6">
-    <div className="grid grid-cols-4 gap-4 items-start pb-6">
-      <div className="col-span-4 sm:col-span-2 flex md:flex">
+    <div className="grid grid-cols-4 gap-2 items-start pb-4">
+      <div className="col-span-4 sm:col-span-3">
         <MyImage
           alt="Portrett pic of me"
           isExpandable={true}
           priority={true}
           src="me.jpg"
           padding={false}
-          // width="540" // I put a size to the img, even though its sized by the parent div, to speed up first contenfu  l load.
+        // width="540" // I put a size to the img, even though its sized by the parent div, to speed up first contenfu  l load.
         />
       </div>
-      <div className="space-y-1 col-span-4 sm:col-span-3">
+      <div className="space-y-1 col-span-4 sm:col-span-4">
         <h1 className="text-lg -mb-1 font-bold">Aleksander Tidemann</h1>
         <p>Music technologist and developer</p>
         <p className="text-secondary dark:text-secondary-dark text-sm">
