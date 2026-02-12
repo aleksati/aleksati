19911b9131e6b102ce117475eeef6844c074b0ba
Aleksander Tidemann <aleksandertid@gmail.com>
2026-02-12 10:30:32 +0100

    Updated publications list

--- src/templates/Publications.tsx
+++ src/templates/Publications.tsx
@@ -45,8 +45,9 @@ const Publications = () => {
       ) : (
         <div className="flex flex-col space-y-6">
           {researchData?.map((item, idx) => {
+            console.log(item)
             // Remove certian results, if needed. Here I remove the KD audio duplicate.
-            if (item.cristin_result_id === "10310729") return;
+            if (item.cristin_result_id === "10268340") return;
             // Remove also Hybrid Learning duplicate.
             if (item.cristin_result_id === "2044269") return;
             // Remove also the duplicate Hardanger Fiddle papers
