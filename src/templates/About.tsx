85963f4d31b76b081376c4908e9e696304cc1599
Aleksander Tidemann <aleksandertid@gmail.com>
2026-03-22 11:08:16 +0100

    more updates

--- src/templates/About.tsx
+++ src/templates/About.tsx
@@ -28,25 +28,25 @@ const About = () => (
       <div className="space-y-4 col-span-2 md:col-span-1">
         <div className="flex flex-col space-y-1 items-center justify-center md:items-start">
           <h1 className="font-bold text-lg -mb-1">Aleksander Tidemann</h1>
-          <p>Music Technologist</p>
-          <MySoMeBar />
-          <p className="text-secondary dark:text-secondary-dark text-sm">
-            @aleksati @alexfürimmer
-          </p>
+          <div>
+            <p>Music Technologist</p>
+            <MySoMeBar />
+            <p className="text-secondary dark:text-secondary-dark text-sm">
+              @aleksati @alexfürimmer
+            </p>
+          </div>
         </div>
         <p>
-          My name is Aleksander and I'm a musician and senior engineer in music
-          technology at the Department of Musicology, University of Oslo. My
+          My name is Aleksander and I'm a senior engineer and lecturerer in music technology at the Department of Musicology, University of Oslo. My
           work sits at the intersection between sound, software, and science,
-          with a particular focus on audio programming and networked sound.
-          Outside of work, I play drums, produce music with friends, and try to
-          read a little every day. This website is both a portfolio and a blog,
-          a place where I share projects and write about music technology and
-          related explorations.
+          with a particular focus on audio programming, networked sound and studios. Outside of work, I play drums, produce music for
+          various acts, and try to read a little every day. This website is both
+          a portfolio and a blog, a place where I share projects and write about
+          music technology and related explorations.
         </p>
         <p>
           Stay updated on my posts by subscribing to my RSS feed from the
-          menubar. You can also reach out by leaving comments under each post.
+          menubar. You can also get in touch by leaving comments under my posts.
         </p>
         {/* <div className="flex flex-col items-center justify-center md:justify-start">
           <p className="font-bold text-lg">Social</p>
@@ -56,8 +56,8 @@ const About = () => (
             @aleksati @alexfürimmer
           </p>
         </div> */}
-        <div className="flex flex-col space-y-2 items-center md:items-start">
-          <p className="font-bold text-lg">Teachings</p>
+        <div className="flex flex-col space-y-2 items-start">
+          <p className="">Current and past university teachings:</p>
           <MyLink href="https://www.uio.no/studier/emner/hf/imv/MUS2850/">
             MUS2850 - Computer Music
           </MyLink>
